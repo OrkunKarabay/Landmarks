7fe09c5758029f92e2c7e61b107b6f88f3625fdf
OrkunKarabay <orkunbarti@gmail.com>
2024-10-15 16:14:35 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Owsito on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
